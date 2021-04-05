eacf9bdc3b601278c87158e51cb701b4820f976c
Kiss Olivér <h662798@stud.u-szeged.hu>
2021-04-05 19:41:16 +0200

    erased movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MovieModule } from './pages/movie/movie.module';
-// import { MovieComponent } from './pages/movie/movie.component';
-
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MovieModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
